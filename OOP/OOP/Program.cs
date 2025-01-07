0278de168c4724194b660f35ffe07646c646ad03
Muhammad Rizwan Tahir <103589890+ChouhdryRizwan@users.noreply.github.com>
2025-01-07 23:03:05 +0500

    classAndobject

---
+++ OOP/OOP/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car newcar = new Car();
+            Console.WriteLine("{0} is the model of {1} company.", newcar.model, newcar.company);
+
+            Console.ReadKey();
+        }
+    }
+
+}
