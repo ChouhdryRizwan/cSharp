415424e17f15311e102a952ee53d5e9b3570330e
Muhammad Rizwan Tahir <103589890+ChouhdryRizwan@users.noreply.github.com>
2025-01-21 12:35:54 +0500

    .net core mvc 8 1

---
+++ exception/exception/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exception
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("num 1");
+           int value1 =  int.Parse(Console.ReadLine());
+
+            Console.WriteLine("num 2");
+            int value2 = int.Parse(Console.ReadLine());
+            try {
+                Console.WriteLine(value1 / value2);
+            }
+            catch {
+                Console.WriteLine("divide nai hu sakta");
+            }
+        }
+    }
+}
---
+++ prac-1/prac-1/Controllers/MenController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace prac_1.Controllers
+{
+    public class MenController : Controller
+    {
+        public string test() {
+            return "this is a testing method";
+            
+        }
+        public IActionResult jeans()
+        {
+            return View();
+            
+        }
+
+        public IActionResult kurta()
+        {
+            return View();
+
+        }
+    }
+}
