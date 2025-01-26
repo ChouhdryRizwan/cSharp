6b29639e4c6d65e15fe742edbbf3b69212570778
Muhammad Rizwan Tahir <103589890+ChouhdryRizwan@users.noreply.github.com>
2025-01-27 00:50:32 +0500

    database-1

---
+++ ModelApp/ModelApp/Models/product.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ModelApp.Models
+{
+    public class product
+    {
+        [Key]
+        public int c_id { get; set; }
+
+        [Required]
+        public string name { get; set; }
+    }
+}
