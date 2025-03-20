1928fb5f5120015c53993581bce463dc1cad98df
Muhammad Rizwan Tahir <103589890+ChouhdryRizwan@users.noreply.github.com>
2025-03-20 06:51:24 +0500

    Iterators

---
+++ Genrics/Genrics/Iterators.cs
@@ -0,0 +1,32 @@
+﻿static IEnumerable<int> GetNumbers()
+{
+    yield return 1;
+    yield return 2;
+    yield return 3;
+}
+
+
+foreach (int num in GetNumbers())
+{
+    Console.WriteLine(num);
+}
+
+
+static IEnumerable<int> GetNumbers6()
+{
+    for (int i = 1; i <= 10; i++)
+    {
+        if (i > 6)
+        {
+            yield break;  // Stop iteration if number is greater than 6
+        }
+
+        yield return i;
+    }
+}
+
+
+foreach (int num in GetNumbers6())
+{
+    Console.WriteLine(num);
+}
---
+++ Genrics/Genrics/Program.cs
@@ -0,0 +1,44 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Box<int> intBox = new Box<int>();  // Works with int
+        intBox.Set(5);
+        Console.WriteLine(intBox.Get());   // Output: 5
+
+        Box<double> stringBox = new Box<double>();  // Works with string
+        stringBox.Set(5.5);
+        Console.WriteLine(stringBox.Get());  // Output: Hello
+
+
+        // Generic Mehtods
+
+        static void Print<T>(T value)  // Generic method
+        {
+            Console.WriteLine(value);
+        }
+
+        static void Main()
+        {
+            Print(10);       // int
+            Print("Hello");  // string
+            Print(3.14);     // double
+        }
+    }
+}
+
+class Box<T> // <T> means generic type
+{
+    private T size;
+
+    public void Set(T newItem)
+    {
+        size = newItem;
+    }
+
+    public T Get()
+    {
+        return size;
+    }
+}
+
