8b85ac863e0d54d1752f2ee1dbe5995d18cd9906
Muhammad Rizwan Tahir <103589890+ChouhdryRizwan@users.noreply.github.com>
2025-04-05 08:51:52 +0500

    advance concepts

---
+++ AdvanceConcepts/AdvanceConcepts/Anonymous.cs
@@ -0,0 +1,27 @@
+﻿//using System;
+
+//public class Program
+//{
+//    public delegate void ShowMessage();
+//    public delegate int Operation(int a, int b);
+
+//    public static void Main()
+//    {
+//        // Anonymous method assigned to delegate
+//        ShowMessage show = delegate ()
+//        {
+//            Console.WriteLine("Hello, this is an anonymous method!");
+//        };
+
+//        // Calling the delegate
+//        show();
+
+//        Operation add = delegate (int x, int y)
+//        {
+//            return x + y;
+//        };
+
+//        int result = add(10, 20);
+//        Console.WriteLine("Addition Result: " + result);
+//    }
+//}
---
+++ AdvanceConcepts/AdvanceConcepts/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+public class Program
+{
+
+
+    public static void Main()
+    {
+        string msg = "Hello from ChatGPT";
+        int count = msg.WordCount();  // Bas aise!
+
+        Console.WriteLine(count);  // Output: 3
+
+    }
+}
+
+public static class MyExtensions
+{
+    public static int WordCount(this string str)
+    {
+        return str.Split(' ').Length;
+    }
+}
+
