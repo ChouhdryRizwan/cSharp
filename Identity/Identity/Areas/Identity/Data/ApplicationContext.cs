bb20eb52894a47b59c8e0fa0cf8b4693166adf08
Muhammad Rizwan Tahir <103589890+ChouhdryRizwan@users.noreply.github.com>
2025-04-08 20:52:03 +0500

    Identity done

---
+++ Identity/Identity/Areas/Identity/Data/ApplicationContext.cs
@@ -0,0 +1,35 @@
+﻿using Identity.Areas.Identity.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Identity.Areas.Identity.Data;
+
+public class ApplicationContext : IdentityDbContext<ApplicationUser>
+{
+    public ApplicationContext(DbContextOptions<ApplicationContext> options)
+        : base(options)
+    {
+    }
+
+
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        // Customize the ASP.NET Identity model and override the defaults if needed.
+        // For example, you can rename the ASP.NET Identity table names and more.
+        // Add your customizations after calling base.OnModelCreating(builder);
+        builder.ApplyConfiguration(new ApplicationUserIdentityConfiguration());
+    }
+}
+
+internal class ApplicationUserIdentityConfiguration : IEntityTypeConfiguration<ApplicationUser>
+{
+    public void Configure(EntityTypeBuilder<ApplicationUser> builder)
+    {
+        builder.Property(x => x.fname).HasMaxLength(120);
+        builder.Property(x => x.lname).HasMaxLength(120);
+    }
+}
