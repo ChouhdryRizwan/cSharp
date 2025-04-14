bd8f3ed37b33adcc1c2f3b18c8fe48bfd2d0f847
Muhammad Rizwan Tahir <103589890+ChouhdryRizwan@users.noreply.github.com>
2025-04-14 07:08:45 +0500

    webapi basic

---
+++ basicAPI/basicAPI/Controllers/StudentController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace basicAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        public List<string> students = new List<string>() {
+        "Osama",
+        "Ali",
+        "Zubair"
+        };
+
+        [HttpGet]
+        public List<string> GetStudents()
+        {
+            return students;
+        }
+
+        [HttpGet("id")]
+        public string GetStudentDetail(int id)
+        {
+            return students[id];
+        }
+    }
+}
